586ca810286052877e7e2442425a08c39d22045c
webpn <stefano@toffolo.info>
2016-10-17 10:43:06 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+function updateObject(target, source){
+	for (var property in source) {
+		if (source.hasOwnProperty(property)) {
+			if (["number", "string", "boolean", "undefined", "symbol", "function"].indexOf(typeof source[property]) >= 0) {
+				target[property] = source[property];
+			}else if(typeof source[property] == "object"){
+				if(!target[property]){
+					target[property] = {};
+				}
+				updateObject(target[property], source[property]);
+			}else{
+				console.log("Unhandled OBJ", typeof source[property]);
+			}
+		}
+	}
+}
